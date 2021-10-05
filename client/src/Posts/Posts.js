ff1391364e34db6e1861b81fafbcc2d47a6f88df
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-05 14:17:51 +0530

    form added

---
+++ client/src/Posts/Posts.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Post from "./Post/Post";
+import { useSelector } from "react-redux";
+import useStyles from "./styles";
+
+const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  console.log(posts);
+  return (
+    <div>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
