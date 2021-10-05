52a76460d04d0676123bb86401031bc953bdb337
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-05 10:47:12 +0530

    Frount end structure created

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,43 @@
-import React, { useEffect } from "react";
-import { AppBar, Container, Grid, Grow, Typography } from "@material-ui/core";
-import memories from "./images/memories.png"; // issue 1
-import Posts from "./Components/Posts/Posts";
-import Form from "./Components/Form/Form";
+import React, { useState, useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+// import { useDispatch } from "react-redux";
+
+// import Posts from "./components/Posts/Posts";
+// import Form from "./components/Form/Form";
+// import { getPosts } from "./actions/posts";
 import useStyles from "./styles";
-import { useDispatch, useSelector } from "react-redux";
-import getPost from "./actions/posts";
+import memories from "./images/memories.png";
 
 const App = () => {
+  //   const [currentId, setCurrentId] = useState(0);
+  //   const dispatch = useDispatch();
   const classes = useStyles();
-  const dispatch = useDispatch();
 
-  const posts = useSelector((state) => state.posts);
-  console.log(posts);
-
-  useEffect(() => {
-    dispatch(getPost());
-  }, [dispatch]);
+  //   useEffect(() => {
+  //     dispatch(getPosts());
+  //   }, [currentId, dispatch]);
 
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit" className={classes.appBar}>
-        <Typography variant="h3" align="center" className={classes.heading}>
-          Memories
-          <image
-            src={memories}
-            alt="memories"
-            height="60"
-            className={classes.image}
-          />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Posts (CRUD)
         </Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
           <Grid
             container
             justify="space-between"
-            align-items="streach"
+            alignItems="stretch"
             spacing={3}
           >
             <Grid item xs={12} sm={7}>
-              <Posts />
+              {/* <Posts setCurrentId={setCurrentId} /> */}
             </Grid>
-            <Grid item xs={12} sm={7}>
-              <Form />
+            <Grid item xs={12} sm={4}>
+              {/* <Form currentId={currentId} setCurrentId={setCurrentId} /> */}
             </Grid>
           </Grid>
         </Container>
