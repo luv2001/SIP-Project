d6124f55b6788c0a07b7f0143e94ffc852c04e4a
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-05 14:33:02 +0530

    create post request done

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
               <Posts />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
