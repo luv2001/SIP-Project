391b972ed099ce2b334982ce986139bb393f6d20
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-06 22:29:07 +0530

    make it better

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("API");
+});
 app.use("/posts", postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
